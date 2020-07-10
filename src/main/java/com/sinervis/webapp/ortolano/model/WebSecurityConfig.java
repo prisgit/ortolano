4cc8aedc3f8244aa580f00e52b9d2a7f87f06913
PRIS <stefano.pastori@tiscali.it>
2020-07-10 11:10:53 +0200

    commit users

---
+++ src/main/java/com/sinervis/webapp/ortolano/model/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.sinervis.webapp.ortolano.model;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/").permittAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+}
